e9c09eb80eac257814e24f21ec58471d01ee4263
JamShootMusic <121457377+JamShootMusic@users.noreply.github.com>
2023-01-01 20:33:59 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+const btn = document.getElementById('button');
+
+document.getElementById('form')
+ .addEventListener('submit', function(event) {
+   event.preventDefault();
+
+   btn.value = 'Sending...';
+
+   const serviceID = 'service_3moyznp';
+   const templateID = 'template_728f5wr';
+
+   emailjs.sendForm(serviceID, templateID, this)
+    .then(() => {
+      btn.value = 'Send Email';
+      alert('Sent!');
+    }, (err) => {
+      btn.value = 'Send Email';
+      alert(JSON.stringify(err));
+    });
+});
